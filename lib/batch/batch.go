6b06d99755b760a00665aa48b8990ec0b35c1e07
Artem Pistryi <gazated@gmail.com>
2022-07-11 09:52:16 +0300

    try1.0

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -14,5 +14,21 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, n)
+	chInt := make(chan int)
+	chUser := make(chan user, n)
+	for i := 0; i < int(pool); i++ {
+		go func(chInt <-chan int, chUser chan<- user) {
+			for {
+				chUser <- getOne(int64(<-chInt))
+			}
+		}(chInt, chUser)
+	}
+	for i := 0; i < int(n); i++ {
+		chInt <- i
+	}
+	for len(res) < int(n) {
+		res = append(res, <-chUser)
+	}
+	return
 }
